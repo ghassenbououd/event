558fb257551c31d2699540d9fd50a3cc7cf2f564
ghassen <ghassen.bououd@levio.ca>
2021-12-11 12:43:25 -0500

    add junit Test

--- src/main/java/com/it/event/EventApplication.java
+++ src/main/java/com/it/event/EventApplication.java
@@ -1,17 +1,47 @@
 package com.it.event;
 
+import com.it.event.model.Evenement;
+import com.it.event.repository.EventRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import java.util.Date;
+
 @SpringBootApplication
 @EntityScan("com.it.event.model")
 @EnableJpaRepositories("com.it.event.repository")
-public class EventApplication {
+public class EventApplication implements CommandLineRunner {
+
 
     public static void main(String[] args) {
         SpringApplication.run(EventApplication.class, args);
+
+    }
+
+
+    @Autowired
+    EventRepository eventRepository;
+
+
+    @Override
+    public void run(String... args) throws Exception {
+
+
+        eventRepository.save(new Evenement(12L ,"Event2", "IT EVENT", new Date(), new Date(), "canada"));
+        eventRepository.save(new Evenement(1L ,"Event3", "IT EVENT", new Date(), new Date(), "canada"));
+        eventRepository.save(new Evenement(10L ,"Event1", "IT EVENT", new Date(), new Date(), "canada"));
+
+        eventRepository.findAll().forEach(p->{
+            System.out.println(p.toString());
+                }
+
+
+        );
     }
 
 }
--- src/test/java/com/it/event/EventApplicationTests.java
+++ src/test/java/com/it/event/EventApplicationTests.java
@@ -1,13 +1,74 @@
 package com.it.event;
 
-import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.Matchers.hasItem;
+
+import com.it.event.model.Evenement;
+import com.it.event.repository.EventRepository;
+import org.junit.jupiter.api.BeforeAll;
+
+
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class EventApplicationTests {
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Date;
+import java.util.List;
+
+import org.junit.Test;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+public class EventApplicationTests {
+
+
+    @Autowired
+    EventRepository eventRepo;
+
+
+    @BeforeAll
     @Test
-    void contextLoads() {
+    public void createEventTest() {
+
+
+        Evenement evenement = new Evenement();
+        evenement.setId(10L);
+        evenement.setEventName("Event1");
+
+        evenement.setEventDescription("IT EVENT");
+        evenement.setLieu("CANADA");
+        evenement.setDebutDate(new Date());
+        evenement.setFinDate(new Date());
+        eventRepo.save(evenement);
+
+        eventRepo.findAll().forEach(p -> {
+            System.out.print("evenement created");
+                    System.out.println(p.toString());
+                }
+        );
+
     }
+
+
+    @BeforeAll
+    @Test
+    public void getAllEventTest() {
+        eventRepo.findAll().forEach(p -> {
+                    System.out.println(p.toString());
+                }
+        );
+        List<Evenement> eventList = eventRepo.findAll();
+
+        //Test equals
+        assertNotEquals(eventList, hasItem(new Evenement(3L, "Event2", "IT EVENT", new Date(), new Date(), "canada")));
+        assertNotNull(eventList);
+
+        // assertThat(eventList, containsInAnyOrder(new Evenement(10L, "event one", "event1", new Date(), new Date(), "Canada")));
+    }
+
 
 }
